a7a97d0371224914fc58e0c77da478484236c899
Mark-Anthony Hutton <mark-anthony.hutton@opsysinc.com>
2018-02-10 15:26:11 -0700

    added templated .gitignore file stubbed python script for advanced lane finding project

---
+++ find_lanes.py
@@ -0,0 +1,16 @@
+# First load a single image and run through pipeline
+
+# define image pre-processing
+
+# define camera calibration
+
+# define distortion logic
+
+# define color thresholding
+
+# define
+
+# Define pipeline
+
+
+
